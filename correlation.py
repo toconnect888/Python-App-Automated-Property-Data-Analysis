0d570384692916c1639d8f6e7acac99c545e91eb
giachen <159936163+toconnect888@users.noreply.github.com>
2025-10-15 14:55:33 -0400

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -31,3 +31,58 @@ print(arr)
 print('number of dimensions :', arr.ndim)
 print('2nd element on 1st row: ', arr[0, 1]) 
 print('Last element from 1st row: ', arr[0, -1])
+
+
+# print(arr[1, 1:4])
+# print(arr[0:2, 1:4]) #slice index 1 to index 4 (not included),
+# print(arr.shape)
+
+# arr = np.array([1, 2, 3, 4, 5])
+# x = arr.copy()
+# arr[0] = 42
+
+# print(arr)
+# print(x)
+
+arr = np.array([1, 2, 3, 4], ndmin=5)
+print('shape of array :',arr.shape)
+
+arr = np.array([1, 2, 3, 4, 5, 6])
+newarr = arr.reshape(2, 3) #(2D array with 2 x 3 elements)
+print(newarr)
+
+for x in np.nditer(newarr):  #print all elements in this newarr array
+  print(x)
+
+for idx, x in np.ndenumerate(newarr): # print position of the element and the value of the element
+  print(idx, x)
+
+for x in np.nditer(newarr[:, ::2]):   #: print values “all rows” ,  “ every second column”
+  print(x)
+
+  
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr_row = np.hstack((arr1, arr2))  #stack on rows
+arr_col = np.vstack((arr1, arr2))  # stack on colomms
+arr_height = np.dstack((arr1, arr2)) #stack on depths
+
+print(arr_row)
+print(arr_col)
+print(arr_height)
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+arr_split_row = np.array_split(arr, 3) # split along rows into three 2D arrays
+arr_split_col = np.hsplit(arr, 3)  # split along coloums into three 2D arrays
+print(newarr)
+
+index_finder = np.where(arr == 4)
+even_index_finder = np.where(arr%2 == 0)
+odd_index_finder = np.where(arr%2 == 1)
+
+randomnumber = random.randint(100, size=(3, 5))  # (3,5) table with any number from 0-100
+randomfloat = random.rand(3, 5)   #(3,5) table with any float number from 0-1
+
+print(randomnumber)
+print(randomfloat)
